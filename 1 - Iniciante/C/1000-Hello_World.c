3d78c2c4b6002f0adc7bdcabbb0744ab5fe763ed
Fernando Costa <90646635+lufffe@users.noreply.github.com>
2023-06-14 22:14:47 -0300

    teste

--- 1 - Iniciante/C/1000-Hello_World.c
+++ 1 - Iniciante/C/1000-Hello_World.c
@@ -1,12 +1,11 @@
-```
+/*
 #Pergunta:
 
 Hello World - O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
 Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
-```
 
-```
 #Resposta:
+*/
 
 #include <stdio.h>           
                                                
@@ -16,4 +15,4 @@ int main() {
                                                
     return 0;                             
 }      
-```
+
